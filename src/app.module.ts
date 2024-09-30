ffffc95c064be957caa742bdf7921015c87687f9
Mohamed Amine Chadli <mohamed.chadli.dev@gmail.com>
2024-09-30 12:22:49 +0100

    config: prisma and config modules and make them global

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,12 @@ import { CartModule } from './modules/cart/cart.module';
 import { OrderModule } from './modules/order/order.module';
 import { ProductModule } from './modules/product/product.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CartModule, OrderModule, ProductModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    PrismaModule, CartModule, OrderModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
